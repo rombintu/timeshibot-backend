c04e7c6950956d7cfb084ddf32db1842d0b3c73b
Nickolsky <romb.intu@gmail.com>
2022-10-03 18:23:56 +0300

    0.1.10

--- store/models.go
+++ store/models.go
@@ -1,8 +1,6 @@
 package store
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
@@ -22,10 +20,31 @@ type Timetable struct {
 
 type Subject struct {
 	gorm.Model
-	TTID    uint      `json:"tt_id"`
-	Time    time.Time `json:"time"`
-	Title   string    `json:"title"`
-	Office  string    `json:"office"`
-	Teacher string    `json:"teacher"`
-	Comment string    `json:"comment"`
+	TTID    uint   `json:"tt_id"`
+	Time    string `json:"time"`
+	Title   string `json:"title"`
+	Office  string `json:"office"`
+	Teacher string `json:"teacher"`
+	Comment string `json:"comment"`
 }
+
+// type JSONTime time.Time
+
+// func (j *JSONTime) UnmarshalJSON(b []byte) error {
+// 	s := strings.Trim(string(b), "\"")
+// 	t, err := time.Parse("1900-01-01T09:00:00", s)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	*j = JSONTime(t)
+// 	return nil
+// }
+
+// func (j JSONTime) MarshalJSON() ([]byte, error) {
+// 	return json.Marshal(time.Time(j))
+// }
+
+// func (j JSONTime) Format(s string) string {
+// 	t := time.Time(j)
+// 	return t.Format(s)
+// }
